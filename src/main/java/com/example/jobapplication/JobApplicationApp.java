ea02a06aaae5ee367df1ca12d3494489c33d8e44
rbanuelospy <robertobanuelos@gmail.com>
2022-05-31 09:40:10 -0400

    initial commit

---
+++ src/main/java/com/example/jobapplication/JobApplicationApp.java
@@ -0,0 +1,13 @@
+package com.example.jobapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JobApplicationApp.class, args);
+  }
+
+}
---
+++ src/main/java/com/example/jobapplication/model/JobApplication.java
@@ -0,0 +1,4 @@
+package com.example.jobapplication.model;
+
+public class JobApplication {
+}
