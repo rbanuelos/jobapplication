1a880bd7c54e95c0a886b4462fb1580a38b57a32
rbanuelospy <robertobanuelos@gmail.com>
2022-06-02 20:59:43 -0400

    refactor to extend jpa instead of crud repository

--- src/main/java/com/example/jobapplication/repository/JobOfferRepository.java
+++ src/main/java/com/example/jobapplication/repository/JobOfferRepository.java
@@ -1,8 +1,8 @@
 package com.example.jobapplication.repository;
 
 import com.example.jobapplication.entity.JobOfferEntity;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JobOfferRepository extends CrudRepository<JobOfferEntity, String> {}
+public interface JobOfferRepository extends JpaRepository<JobOfferEntity, Long> {}
