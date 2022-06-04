1e8f3246c88910fd4a5105db380a80a088e832fb
rbanuelospy <robertobanuelos@gmail.com>
2022-06-03 23:47:50 -0400

    redis config refactor. added support for cache

--- src/main/java/com/example/jobapplication/config/RedisConfig.java
+++ src/main/java/com/example/jobapplication/config/RedisConfig.java
@@ -1,51 +1,49 @@
 package com.example.jobapplication.config;
 
 import java.time.Duration;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisPassword;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericToStringSerializer;
 
-@Configuration
+/**
+ * Redis configuration class. Contains beans to support Redis as a cache. Please take a look at
+ * application.properties files to see redis connection values.
+ */
+@EnableAutoConfiguration
 public class RedisConfig {
-  @Value("${redis.host}")
-  private String host;
 
-  @Value("${redis.port}")
-  private int port;
-
-  @Value("${redis.database}")
-  private int database;
-
-  @Value("${redis.password}")
-  private String password;
-
+  /**
+   * Redis template bean.
+   *
+   * @param connectionFactory connectionFactory
+   * @return RedisTemplate
+   */
   @Bean
-  JedisConnectionFactory jedisConnectionFactory() {
-    RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-    redisStandaloneConfiguration.setHostName(host);
-    redisStandaloneConfiguration.setPort(port);
-    redisStandaloneConfiguration.setDatabase(database);
-    redisStandaloneConfiguration.setPassword(RedisPassword.of(password));
+  public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory connectionFactory) {
+    RedisTemplate<?, ?> template = new RedisTemplate<>();
+    template.setConnectionFactory(connectionFactory);
 
-    JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration =
-        JedisClientConfiguration.builder();
-    jedisClientConfiguration.connectTimeout(Duration.ofSeconds(60)); // 60s connection timeout
-
-    return new JedisConnectionFactory(redisStandaloneConfiguration,
-        jedisClientConfiguration.build());
+    return template;
   }
 
+  /**
+   * Redis Cache Manager class.
+   *
+   * @param connectionFactory connectionFactory
+   * @return RedisCacheManager
+   */
   @Bean
-  public RedisTemplate<String, Object> redisTemplate() {
-    final RedisTemplate<String, Object> template = new RedisTemplate<>();
-    template.setConnectionFactory(jedisConnectionFactory());
-    template.setValueSerializer(new GenericToStringSerializer<>(Object.class));
-    return template;
+  public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+    RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+        .prefixCacheNameWith(this.getClass().getPackageName() + ".")
+        .entryTtl(Duration.ofMinutes(5))
+        .disableCachingNullValues();
+
+    return RedisCacheManager.builder(connectionFactory)
+        .cacheDefaults(config)
+        .build();
   }
 }
