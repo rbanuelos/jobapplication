92fcc94333db47699652825d25fc8a2c509b4fe6
rbanuelospy <robertobanuelos@gmail.com>
2022-06-06 11:45:09 -0400

    added simple context load test

--- src/test/java/com/example/jobapplication/JobOfferAppTests.java
+++ src/test/java/com/example/jobapplication/JobOfferAppTests.java
@@ -1,13 +1,20 @@
 package com.example.jobapplication;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.example.jobapplication.controller.JobOfferRestController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JobOfferAppTests {
 
+  @Autowired
+  private JobOfferRestController jobOfferRestController;
   @Test
   void contextLoads() {
+    assertThat(jobOfferRestController).isNotNull();
   }
 
 }
