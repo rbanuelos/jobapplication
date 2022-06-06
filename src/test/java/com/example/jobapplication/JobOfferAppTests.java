bb9157b4ad51e8bcaed6a418a433023b76afeb8f
rbanuelospy <robertobanuelos@gmail.com>
2022-06-06 15:01:11 -0400

    removed context load for rest controller class

--- src/test/java/com/example/jobapplication/JobOfferAppTests.java
+++ src/test/java/com/example/jobapplication/JobOfferAppTests.java
@@ -1,20 +1,13 @@
 package com.example.jobapplication;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-import com.example.jobapplication.controller.JobOfferRestController;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JobOfferAppTests {
-
-  @Autowired
-  private JobOfferRestController jobOfferRestController;
   @Test
   void contextLoads() {
-    assertThat(jobOfferRestController).isNotNull();
+
   }
 
 }
